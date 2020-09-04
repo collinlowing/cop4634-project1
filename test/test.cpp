c35cd80225b5b621317f081715d57f5ebd044343
Collin Lowing <cel19@students.uwf.edu>
2020-09-03 19:48:08 -0500

    Add test/ [no test cases written]

---
+++ test/test.cpp
@@ -0,0 +1,22 @@
+#define CATCH_CONFIG_MAIN
+
+#include<sstream>
+
+#include "catch/catch.hpp"
+// uncomment and add includes per your program layout
+// #include "../.hpp"
+// #include "../.hpp"
+// #include "../.hpp"
+
+/**************************************************
+  This is a skeleton test file. You should edit
+  the above includes to match header files in your
+  program.
+ **************************************************/
+TEST_CASE("class XXXX")
+{
+  // edit test cases for however many classes you
+  // create
+  REQUIRE(true);
+  REQUIRE(true);
+}
